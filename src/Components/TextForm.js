1de53300e730a408e04fa8a5953845d7b73eda6e
Aditya-Patel24 <adityasp243@gmail.com>
2024-03-09 16:18:51 +0530

    change

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function TextForm(props) {
+export default function TextForm() {
   const onUpper = () => {
     let newText = text.toUpperCase();
     setText(newText);
@@ -12,33 +12,34 @@ export default function TextForm(props) {
   const onClear = () => {
     let newText = "";
     setText(newText);
-    console.log(text.split(" "));
+    console.log(text.split(" "))
   };
   const handleOnchange = (event) => {
     setText(event.target.value);
   };
   const [text, setText] = useState("Enter Text Here");
-  let myP = {
-    color: "cyan",
+let myP={
+color:"cyan",
+border: "1px solid black",
+  }
+  const[mystyle, setMystyle]=useState({
+    color:"black",
     border: "1px solid black",
-  };
-  const [mystyle, setMystyle] = useState({
-    color: "black",
-    border: "1px solid black",
-    backgroundColor: "white",
-  });
-  const [Mode, setMode] = useState();
+    backgroundColor:"white"
+      })
+   const toggleonClick =()=>{
+        if(mystyle.color === "white")
+        setMystyle({
+          color:"black",
+          border: "1px solid black",
+          backgroundColor:"white"
+        })
+        else{
+         setMystyle({ color:"white",
+         backgroundColor:"black"})
+        }
+      }
 
-  const toggleonClick = (cls) => {
-    document.body.classList.add("bg-" + Mode);
-    if (Mode === "light") {
-      setMode("dark");
-      document.body.style.backgroundColor = "black";
-    } else {
-      setMode("light");
-      document.body.style.backgroundColor = "white";
-    }
-  };
 
   return (
     <>
@@ -65,30 +66,13 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-2 my-2" onClick={onClear}>
           Clear
         </button>
-        <button
-          className="btn btn-primary mx-2 my-2"
-          onClick={() => {
-            props.toggleonClick("null");
-          }}
-        >
+        <button className="btn btn-primary mx-2 my-2" onClick={toggleonClick}>
           ChangeMode
         </button>
-        <button
-          className="btn btn-primary mx-2 my-2"
-          onClick={() => {
-            props.toggleonClick("primary");
-          }}
-        ></button>
-        <button
-          className="btn btn-success mx-2 my-2"
-          onClick={() => {
-            props.toggleonClick("success");
-          }}
-        ></button>
         <h3>Your text details</h3>
-        <p>No of Words are {text.split(" ").filter((element)=>{return element.length!==0}).length}</p>
-        <p>No of alphabets are {text.length}</p>
-        <p>Approx {0.008 * text.split(" ").length} Minitues required to read</p>
+        <p>No of Words are {text.split(/\s+/).filter((element)=>{return element.length!==0}).length}</p>
+        <p>No of alphabets are {textfilter((element)=>{return element.length!==0}).length}</p>
+        <p>Approx {0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minitues required to read</p>
         <h3>Your Text Preview Here</h3>
         <p className="mx-2" style={myP}>{text}</p>
       </div>
